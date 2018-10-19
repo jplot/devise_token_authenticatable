332e733372721b6785d0f49b18cace8c0c972d3f
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2018-10-19 09:56:40 +0200

    Adds generators

--- lib/devise_token_authenticatable.rb
+++ lib/devise_token_authenticatable.rb
@@ -1,5 +1,6 @@
 require 'devise_token_authenticatable/controllers/token_authenticatable'
 require 'devise_token_authenticatable/models/token_authenticatable'
 require 'devise_token_authenticatable/strategies/token_authenticatable'
+require 'devise_token_authenticatable/generators/initializer_generator'
 
 Devise.add_module :token_authenticatable, controller: true, model: true, strategy: true, no_input: true
